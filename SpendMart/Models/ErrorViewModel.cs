20194761eaea52a530958ed128bd293ab1df7cad
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-09-25 19:28:41 +0530

    Add project files.

---
+++ SpendMart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpendMart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
